757dd9b2bb751976c860688858020db9c7e9f20a
Shreya Sheel <ssheel@ucsd.edu>
2019-01-31 10:34:15 -0800

    node.js lab

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ var hello = require('./routes/hello');
 // Example route
 // var user = require('./routes/user');
 
+var index = require('./routes/index');
+var project = require('./routes/project');
+
 var app = express();
 
 // all environments
@@ -40,6 +43,10 @@ app.get('/', index.view);
 app.get('/hello/:userName', hello.view);
 // Example route
 // app.get('/users', user.list);
+
+app.get('/', index.view);
+app.get('/project', project.viewProject);
+app.get('/project/:name', project.viewProject);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
